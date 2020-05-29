c08d4ac36c218841b78ae50d40188c740877e9da
CrazyMax <crazy-max@users.noreply.github.com>
2020-05-29 11:04:39 +0200

    Portapps 2.4.4

--- main.go
+++ main.go
@@ -64,5 +64,6 @@ idea.log.path={{ DATA_PATH }}/log`, "{{ DATA_PATH }}", utl.FormatUnixPath(app.Da
 		utl.CopyFile(utl.PathJoin(app.DataPath, vmOptionsFile), utl.PathJoin(app.AppPath, "bin", ideaVmOptionsFile))
 	}
 
+	defer app.Close()
 	app.Launch(os.Args[1:])
 }
